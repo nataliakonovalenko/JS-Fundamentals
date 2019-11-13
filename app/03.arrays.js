8573db5165f3bd9e5b91aee85c809663df8d733d
Natalia_Konovalenko <natalia_konovalenko@epam.com>
2019-11-13 18:32:31 +0200

    JS-Fundamentals: Arrays 18-21 tasks

--- app/03.arrays.js
+++ app/03.arrays.js
@@ -10,6 +10,14 @@ console.log('Topic: Arrays');
 //     2.    with deleting this element from an array.
 //     Display them in the console.
 
+// var array = [1, 2, 3, 4];
+// var lastElement = array[array.length-1];
+// console.log(lastElement);
+// console.log(array);
+// let newLastElement = array.pop();
+// console.log(newLastElement);
+// console.log(array);
+
 // Task 02
 // RU: Создать массив любых элементов. Добавить элемент в конец массива.
 //     1. Модифицировать текущий массив
@@ -19,6 +27,12 @@ console.log('Topic: Arrays');
 //     1. mutate current array;
 //     2. create a new array.
 //     Disply them in the conole.
+
+// var array = [8, 9, 10];
+// array.push(11);
+// console.log(array);
+// var newArray = array.concat([12, 13]);
+// console.log(newArray);
 
 // Task 03
 // RU: Создать массив любых элементов. Вставить новый элемент под индексом 3.
@@ -30,8 +44,11 @@ console.log('Topic: Arrays');
 //     2. create a new array.
 //     Disply them in the conole.
 
-
-
+// var array = [1, 2, 3, 4, 5, 6];
+// array[3] = 10;
+// console.log(array);
+// var newArray = [...array.slice(0, 3), 12, ...array.slice(4)];
+// console.log(newArray);
 
 // Task 04
 // RU: Создать массив любых элементов.
@@ -39,17 +56,36 @@ console.log('Topic: Arrays');
 // EN: Create an array of any elements.
 //     Iterate over this array and display each element in the console.
 
+// let array = [1, 2, 3, 4, 5];
+// array.forEach(function(el) {
+//     console.log(el);
+// });
+
 // Task 05
 // RU: Создать массив чисел в диапазоне от 0 до 100.
 //     Подсчитать и вывети сумму тех элементов, которые больше 50.
 // EN: Create an array of numbers in the range from 0 to 100.
 //     Calculate and display the sum of the elements, which are greater than 50.
 
+// let array = [10, 70, 50, 60, 80];
+// var result = array.filter(function(el){
+//         return el > 50;
+// });
+// console.log(result);
+// let sum = result.reduce(function(start, next){
+//     return start + next;
+// });
+// console.log(sum);
+
 // Task 06
 // RU: Создать массив строк. На основе этого массива создать строку –
 //     объдинить все элементы массива, используя определенный разделитель.
 // EN: Create an array of strings. Create a string on the basis of this array.
 //     This string should contain all elements from an array separated by certain delimeter.
+
+// var array = ['a', 'b', 'c', 'd'];
+// var string = array.join('*');
+// console.log(string);
 
 // Task 07
 // RU: Создать массив чисел от 1 до 20 в случайном порядке.
@@ -58,6 +94,14 @@ console.log('Topic: Arrays');
 // EN: Create an array of numbers in the range from 1 to 20 in random order.
 //     Sort this array in ascending order. Display it in the console.
 //     Create a copy of the previous array in reverse order. Disply it in the console.
+
+// var array = [35, 5, 2, 15, 32];
+// array.sort(function(el1, el2){
+//     return el1 - el2;
+// });
+// console.log(array);
+// var array2 = array.reverse();
+// console.log(array2);
 
 // Task 08
 // RU: Создать массив [3, 0, -1, 12, -2, -4, 0, 7, 2]
@@ -70,6 +114,19 @@ console.log('Topic: Arrays');
 //     First part - negative values from the original array in the same order,
 //     Next part - zeroes
 //     Last part - positive values from the original array in the same order.
+
+// var array = [3, 0, -1, 12, -2, -4, 0, 7, 2];
+// var newArray = array.filter(function(el){
+//     return el < 0;
+// });
+// var newArray2 = array.filter(function(el){
+//     return el === 0;
+// });
+// var newArray3 = array.filter(function(el){
+//     return el > 0;
+// });
+// var newArrray4 = newArray.concat(newArray2, newArray3);
+// console.log(newArrray4);
 
 // Task 09
 // RU: 1. Создайте массив styles с элементами "Jazz", "Blues".
@@ -85,17 +142,37 @@ console.log('Topic: Arrays');
 //     5. Add two new elements "Rap" and "Reggae" at the begining of the array.
 //     6. Display an array in the console.
 
+// var styles = ["Jazz", "Blues"];
+// styles.push("Rock-n-Roll");
+// styles[styles.length-2] = "Classics";
+// var firstElement = styles.shift();
+// console.log(firstElement);
+// styles.unshift("Rap", "Reggae");
+// console.log(styles);
+
 // Task 10
 // RU: Подсчитать в строке "dskjdhfkjshdfkjhsdkjureyteiruyiqywehjkh"
 //     отдельно количество букв r, k, t и вывести в консоль.
 // EN: Calculate the number of letters r, k, t in this string
 //     "dskjdhfkjshdfkjhsdkjureyteiruyiqywehjkh" and display them in the console.
 
+// var string = "dskjdhfkjshdfkjhsdkjureyteiruyiqywehjkh";
+// var sumK = string.split('k').length-1;
+// console.log(sumK);
+// var sumR = string.split('r').length-1;
+// console.log(sumR);
+// var sumT = string.split('t').length-1;
+// console.log(sumT);
+
 // Task 11
 // RU: Создать массив любых элементов.
 //     Получить случайный элемент из массива и вывести его в консоль.
 // EN: Create an array of any elements.
 //     Get the random element from this array and display it in the console.
+
+// var array = [17, 34, 85, 15];
+// var index = Math.floor(Math.random()*array.length);
+// console.log(array[index]);
 
 // Task 12
 // RU: Создать двумерный массив:
@@ -109,11 +186,22 @@ console.log('Topic: Arrays');
 //     7 8 9
 //     Display it in the console.
 
+// var array = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
+//
+// array.forEach((item) => {
+//     console.log(item.join(' '));
+// });
+
+
 // Task 13
 // RU: Преобразовать массив из предыдущего задания в одномерный.
 //     Вывести его в консоль
 // EN: Transform an array from the previous task into one-dementional array.
 //     Display it in the console.
+
+// var array = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
+// var newArray = array.flat(1);
+// console.log(newArray);
 
 // Task 14
 // RU: Создать массив любых элементов.
@@ -125,11 +213,24 @@ console.log('Topic: Arrays');
 //     contain elements from index a to index b.
 //     Display it in the console.
 
+// var array = [1, 2, 3, 4, 5];
+// var newArray = array.slice(1, 4);
+// console.log(newArray);
+
 // Task 15
 // RU: Создать массив любых элементов.
 //     Найти индекс указаного элемента в массиве и вывести его в консоль.
 // EN: Create an array of any elements.
 //     Find the index of a particular element in the array and disply it in the console.
+
+// var array = [1, 2, 3, 4, 5];
+// var indexElem = array.indexOf(4);
+// console.log(indexElem);
+//
+// var findIndexElem = array.findIndex(function(value) {
+//     return value === 5;
+// });
+// console.log(findIndexElem);
 
 // Task 16
 // RU: Создать массив с дублями элементов. На его основе создать новый массив
@@ -139,6 +240,16 @@ console.log('Topic: Arrays');
 //     Remove duplicated elements.
 //     Display it in the console.
 
+// var array = [2, 2, 7, 0, 9, 8, 7];
+// var newArray = [];
+// array.forEach(function(item){
+//     if(newArray.indexOf(item) < 0){
+//         newArray.push(item);
+//     }
+// });
+//
+// console.log(newArray);
+
 // Task 17
 // RU: Создать массив с дублями. Найти первый элемент, который дублируется.
 //     Заменить этот элемент и все его копии на символ '*'.
@@ -146,6 +257,22 @@ console.log('Topic: Arrays');
 // EN: Create an array with duplicate elements. Find first duplicated element.
 //     Replace this element and all its copies with symbol '*'.
 //     Display it in the console.
+
+// var array = [2, 2, 7, 0, 9, 8, 7];;
+// var fin = false;
+// var result;
+// for(let value of array){
+//     if(array.indexOf(value) !== array.lastIndexOf(value)){
+//         result = value;
+//         array.forEach(function(element, index, arr) {
+//             if (element === value) {
+//                 arr[index] = '*';
+//             }
+//         });
+//         break;
+//     }
+// }
+// console.log(array);
 
 // Task 18
 // RU: Создать массив целых чисел. На его основе создать массивы – представления
@@ -157,6 +284,23 @@ console.log('Topic: Arrays');
 //     Third array contains the hexadecimal representation of the elements from the original array.
 //     Display them in the console.
 
+console.log('Task 18');
+var array = [10, 17, 34, 77, 35, 7];
+var newArray = array.map(function(el) {
+    return el.toString(2);
+});
+console.log(newArray);
+
+var newArray2 = array.map(function(el) {
+    return el.toString(8);
+});
+console.log(newArray2);
+
+var newArray3 = array.map(function(el) {
+    return el.toString(16);
+});
+console.log(newArray3);
+
 // Task 19
 // RU: Получить из строки 'a big brown fox jumps over the lazy dog' массив слов,
 //     который содержит элементы, длина которых не больше 3 символа.
@@ -165,6 +309,14 @@ console.log('Topic: Arrays');
 //     This array should contain only words, the length of which is 3 or less characters.
 //     Display it in the console.
 
+console.log('Task 19');
+var string = 'a big brown fox jumps over the lazy dog';
+var arrayFromString = string.split(' ');
+var newArrayFromString = arrayFromString.map(function(el) {
+    return el.slice(0, 3);
+});
+console.log(newArrayFromString);
+
 // Task 20
 // RU: Создать массив, который содержит строки и числа.
 //     Проверить, содержит ли массив только строки.
@@ -172,6 +324,13 @@ console.log('Topic: Arrays');
 // EN: Create an array of numbers and strings.
 //     Check whether this array contains only strings.
 //     Display the result in the console.
+
+console.log('Task 20');
+var array2 = [1, 'one', 89, 'two', 7];
+var isString = array2.every(function(el) {
+    return el.length;
+});
+console.log(isString);
 
 // Task 21
 // RU: Создать отсортированный массив чисел.
@@ -184,3 +343,25 @@ console.log('Topic: Arrays');
 //     and a value and returns the index of this value in the array or -1.
 //     Function should use binary search.
 //     Display the result in the console.
+
+console.log('Task 21');
+var sortArr = [1, 5, 7, 9, 16, 24, 75, 100, 102, 105, 110];
+
+function binarySearch(arr, value) {
+    var midElemIndex = Math.floor(arr.length/2);
+    var midElem  = arr[midElemIndex];
+
+    if (midElem === value) {
+        return midElemIndex;
+    } else if (arr.length === 1) {
+        return -1;
+    } else if (midElem > value) {
+        return binarySearch(arr.slice(0, midElemIndex), value);
+    } else {
+        var resultIndex = binarySearch(arr.slice(midElemIndex), value);
+        return resultIndex === -1 ? -1 : resultIndex + midElemIndex;
+    }
+}
+
+console.log( binarySearch(sortArr, 25));
+console.log( binarySearch(sortArr, 100));
